b9d21f63dd571745e55901c8865280ba5aa27976
Kuldeep <kuldeepsmarttech@gmail.com>
2026-06-13 03:44:43 +0530

    Update utils.py

--- core/utils.py
+++ core/utils.py
@@ -1,1 +1,30 @@
+from datetime import datetime
+from urllib.parse import urlparse
+import os
 
+def get_timestamp():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+def normalize_url(url):
+    if not url.startswith(("http://", "https://")):
+        return "https://" + url
+    return url
+
+def get_domain(url):
+    return urlparse(url).netloc.replace(":", "_")
+
+def create_output_directory(domain):
+    path = os.path.join("output", domain)
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    return path
+
+SEVERITY_SCORE = {
+    "Critical": 10,
+    "High": 7,
+    "Medium": 5,
+    "Low": 2,
+    "Info": 1
+}
